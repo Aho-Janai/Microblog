7d527a750ed8147ac323bd95a3be2011a1570960
Aho-Janai <msmangekyou49@gmail.com>
2026-08-13 17:00:06 -0500

    Pagination has been added

--- config.py
+++ config.py
@@ -11,4 +11,5 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['msmangekyou49@gmail.com']
+    POSTS_PER_PAGE = 3
 
